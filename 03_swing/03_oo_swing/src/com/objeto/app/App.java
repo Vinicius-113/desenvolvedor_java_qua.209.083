81c8878408ac60242024c417a327622b042b47d2
Vinicius-113 <mv6823117@gmail.com>
2025-11-11 21:47:52 -0300

    Aula 11/11/2025

---
+++ 03_swing/03_oo_swing/src/com/objeto/app/App.java
@@ -0,0 +1,33 @@
+package com.objeto.app;
+
+import javax.swing.JOptionPane;
+
+import com.objeto.models.Pessoa;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Pessoa usuario = new Pessoa();
+
+        // entrada de dados
+
+        usuario.setNome(JOptionPane.showInputDialog("Informe seu nome:"));
+        usuario.setIdade(Integer.parseInt(JOptionPane.showInputDialog("Informe sua idade:")));
+        usuario.setCpf(JOptionPane.showInputDialog("Informe seu cpf:"));
+    
+        usuario.setEmail(JOptionPane.showInputDialog("Informe seu email:"));
+    
+        usuario.setTelefone(JOptionPane.showInputDialog("Informe seu telefone:"));
+
+        // saida de dados
+        JOptionPane.showMessageDialog(null, "Dados do Usuario:\nNome: " + usuario.getNome() + 
+        "\nIdade: " + usuario.getIdade() +
+        "\nCPF: " + usuario.getCpf() +
+        "\nE-mail: " + usuario.getEmail() +
+        "\nTelefone: " + usuario.getTelefone()
+        );
+    
+
+
+    }
+}
---
+++ 03_swing/04_combustivel/src/com/combustivel/models/app/App.java
@@ -0,0 +1,46 @@
+package com.combustivel.models.app;
+
+import javax.swing.JOptionPane;
+
+import com.combustivel.models.Combustivel;
+import com.combustivel.models.Combustivel;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Combustivel combustivel = new Combustivel();
+
+        // array 
+        String[] opcoes = {"Informar Valores", "Sair do programa"};
+        Object opcao;
+
+        do {
+
+            
+            // entrada de dados 
+            opcao = JOptionPane.showInputDialog(null,
+            "Selecione uma Opção:",
+            "combustivel",
+            0,
+            null,
+            opcoes,
+            opcoes[0]
+            );
+
+           
+                
+            if (opcao != "Sair do Programa") {
+
+                
+                
+                combustivel.setGasolina(Double.parseDouble(JOptionPane.showInputDialog ("Informe o Valor da Gasolina:").replace(",", ".")));
+                combustivel.setEtanol(Double.parseDouble(JOptionPane.showInputDialog("Informe o valor do etanol:").replace(",", ".")));
+                
+                // saida de dados 
+                JOptionPane.showMessageDialog(null, combustivel.calcularCombustivel());  
+            }
+        } while (opcao != "Sair do programa");
+
+
+    }
+}
