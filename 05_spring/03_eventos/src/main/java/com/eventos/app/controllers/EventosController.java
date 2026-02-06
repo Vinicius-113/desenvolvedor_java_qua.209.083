d80fd1b1ca42ac78e72cc0ae6528953e5c96e33a
Vinicius-113 <mv6823117@gmail.com>
2026-02-05 21:42:47 -0300

    Aula do dia 05/02/2026

--- 05_spring/03_eventos/src/main/java/com/eventos/app/controllers/EventosController.java
+++ 05_spring/03_eventos/src/main/java/com/eventos/app/controllers/EventosController.java
@@ -2,26 +2,28 @@ package com.eventos.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult; // NOTE
+import org.springframework.validation.annotation.Validated; // NOTE
+import org.springframework.web.bind.annotation.PathVariable; // NOTE
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes; // NOTE
 
 import com.eventos.app.models.Evento;
 import com.eventos.app.repository.EventosRepository;
-
 
 @Controller
 public class EventosController {
     @Autowired
     private EventosRepository csr;
 
-    @RequestMapping(value= "/", method=RequestMethod.GET)
+    @RequestMapping(value="/", method=RequestMethod.GET)
     public ModelAndView index() {
         ModelAndView mv = new ModelAndView("index");
-         Iterable<Evento> eventos = csr.findAll();
-         mv.addObject("eventos", eventos);
-         return mv;
-        
+        Iterable<Evento> eventos = csr.findAll();
+        mv.addObject("eventos", eventos);
+        return mv;
     }
 
     @RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
@@ -40,5 +42,22 @@ public class EventosController {
         return "cadastro-sucesso";
     }
 
-    
+    @RequestMapping(value="/editarEvento/{idEvento}", method=RequestMethod.GET)
+    public ModelAndView editarEvento(@PathVariable("idEvento") long idEvento) {
+        Evento evento = csr.findByIdEvento(idEvento);
+        ModelAndView mv = new ModelAndView("editar-evento");
+        mv.addObject("evento", evento);
+        return mv;
+    }
+
+    @RequestMapping(value="/editarEvento/{idEvento}", method=RequestMethod.POST)
+    public String editarEvento(@Validated Evento evento, BindingResult result, RedirectAttributes attributes) {
+        csr.save(evento);
+        return "redirect:/alteracaoSucesso";
+    }
+
+    @RequestMapping("/alteracaoSucesso")
+    public String alteracaoSucesso() {
+        return "editar-sucesso";
+    }
 }
